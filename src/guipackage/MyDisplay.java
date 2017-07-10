851d239b3e19d64a1cef9303a19896b31ea7c2e4
Igor Prus <amitator@gmail.com>
2017-07-10 18:48:11 +0300

    minor

---
+++ src/guipackage/MyDisplay.java
@@ -0,0 +1,19 @@
+package guipackage;
+
+import processing.core.PApplet;
+
+/**
+ * Created by Igort on 10.07.2017.
+ * To Display window with background picture
+ */
+public class MyDisplay extends PApplet {
+    public void setup(){
+        size(400, 400);
+        background();
+    }
+
+    public void draw(){
+
+    }
+
+}
